be503bc08c8febf8701738de8c599eca7aa19793
bijink <kbijin528@gmail.com>
2021-09-13 19:18:23 +0530

    next and back button for video browsing in MovieDetails page added

--- src/Components/MovieDetails/MovieDetails.jsx
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -7,37 +7,51 @@ import axios from '../../Axios';
 import VideoPopUp from '../../PopUps/VideoPopUp/VideoPopUp';
 import { VideoPopUpCC } from '../../Store/VideoPopUpContext';
 import YoutubeEmbed from '../YoutubeEmbed/YoutubeEmbed';
-// import YoutubeEmbed from '../../YoutubeEmbed';
 
 const MovieDetails = () => {
    const { details } = useContext(MovieDetailsCC);
-   console.log(details);
+   // console.log(details);
    const [urlId, setUrlId] = useState();
    // console.log(urlId);
    const { videoPopUpTrigger, setVideoPopUpTrigger } = useContext(VideoPopUpCC);
+   var [count, setCount] = useState(0);
+   const [lg, setLg] = useState();
+   // console.log(count);
+   // console.log(videoNext_Back);
 
    useEffect(() => {
       setVideoPopUpTrigger(false);
+      return () => {
+      };
    }, []);
 
-   const handleVideo = () => {
-      console.log(details.id);
+
+
+   const handleVideo = (aa) => {
+      if (aa === 'back') {
+         if (count == 0)
+            setCount(count = (lg - 1));
+         else
+            setCount(count -= 1);
+      } else if (aa === 'next') {
+         if (count < (lg - 1))
+            setCount(count += 1);
+         else
+            setCount(count = 0);
+      }
+      // console.log(details.id);
       axios.get(`/movie/${details.id}/videos?api_key=${API_KEY}&language=en-US`).then(res => {
          if (res.data.length !== 0) {
-            console.log(res.data.results[0]);
+            // console.log(res.data.results[0]);
+            // console.log(res.data.results);
             setVideoPopUpTrigger(true);
-            setUrlId(res.data.results[0]);
+            setUrlId(res.data.results[count]);
+            setLg(res.data.results.length);
          }
       });
    };
 
-   const opts = {
-      height: '533',
-      width: '100%',
-      playerVars: {
-         autoplay: 1,
-      },
-   };
+
 
    return (
       <div className="parentDivMovieDetails" style={{ backgroundImage: `url(${details && (imageUrl + details.backdrop_path)})` }}>
@@ -53,7 +67,8 @@ const MovieDetails = () => {
                <p>{details.release_date && `Release Date : ${details.release_date}`} </p>
                <p>{details.vote_average && `Rating (average) : ${details.vote_average} / 10`} </p>
                <br />
-               <h2 onClick={handleVideo} className="Teaser" ><i className={`${details.id && 'fab fa-youtube'}`}></i> {details.id && 'Teaser / Making'}</h2>
+               <h2 onClick={() => handleVideo()} className="Teaser" ><i className={`${details.id && 'fab fa-youtube'}`}></i> {details.id && 'Teaser / Making'}</h2>
+               {/* <h2 onClick={() => aa} className="Teaser" ><i className={`${details.id && 'fab fa-youtube'}`}></i> {details.id && 'Teaser / Making'}</h2> */}
             </div>
          </div>
          {
@@ -63,8 +78,12 @@ const MovieDetails = () => {
                   {urlId && <YouTube videoId={urlId.key} opts={opts} />}
                </div> */}
                {urlId ? < YoutubeEmbed embedId={urlId.key} /> : <h1 className="noVideo">Video is not available</h1>}
+               <i className="videoBackbtn fas fa-chevron-circle-left" onClick={() => handleVideo('back')} ></i>
+               <i className="videoNextbtn fas fa-chevron-circle-right" onClick={() => handleVideo('next')} ></i>
             </VideoPopUp>
          }
+         {/* <button onClick={() => handleVideo('back')} >back</button>
+         <button onClick={() => handleVideo('next')} >next</button> */}
       </div>
    );
 };
--- src/PopUps/VideoPopUp/VideoPopUp.jsx
+++ src/PopUps/VideoPopUp/VideoPopUp.jsx
@@ -5,16 +5,16 @@ import './VideoPopUp.css';
 
 const VideoPopUp = (props) => {
    const { setVideoPopUpTrigger } = useContext(VideoPopUpCC);
-   const ee = props.shade;
+   // const ee = props.shade;
 
    return (
       // <div className="parentVideoPopUp" style={{ backgroundColor: props.shade ,width:props.width,height:props.height,top:props.top,left:props.left,}}>
       <div className={props.shade ? 'parentVideoPopUp_banner' : 'parentVideoPopUp'}>
          <div className="close">
-            <i onClick={() => { setVideoPopUpTrigger(false); }} className="fas fa-times-circle"></i>
+            <i onClick={() => setVideoPopUpTrigger(false)} className="fas fa-times-circle"></i>
          </div>
          {props.children}
-      </div>
+      </div >
    );
 };
 
