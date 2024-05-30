dca79ef9263fb6ab867544cc556621c78c7a4752
Bijin Kandengala <kbijin528.pro@gmail.com>
2024-05-30 17:44:26 +0530

    Add api fail handling

--- src/Hooks/useMoviesData.js
+++ src/Hooks/useMoviesData.js
@@ -8,46 +8,69 @@ export const useMoviesData = (url, keyID) => {
       async () => {
          if (keyID === "banner") {
             // #*to return the stored banner data(from sessionStorage) for improving performance
-            const bannerFetchData = JSON.parse(sessionStorage.getItem("netflix_banner_query_data"));
-            if (bannerFetchData) return bannerFetchData;
+            const bannerQueryData = JSON.parse(sessionStorage.getItem("netflix_banner_query_data"));
+            if (bannerQueryData) return bannerQueryData;
 
             let page = 1;
             let moviesData_limit = 15;
             let finalMoviesData = [];
-            let finalizedFetchData = {};
+            let finalizedBannerData = {};
+
+            function fetchData(pageNo) {
+               return new Promise(async (resolve, reject) => {
+                  // #setting a timeout to handle 504 error
+                  setTimeout(() => {
+                     reject();
+                  }, 10 * 1000); // #10 seconds
+
+                  await axios_instance
+                     .get(`${url}&page=${pageNo}`)
+                     .then((res) => {
+                        resolve(res);
+                     })
+                     .catch((err) => {
+                        reject(err);
+                     });
+               });
+            }
 
             // *to make all movies of banner that has video
             // #movies which doesn't have videos will removed
             // #if there is no movies available that has videos in the fetch, then the fetch process will repeat until 15 data get
             do {
-               const fetchData = await axios_instance.get(`${url}&page=${page}`);
-               // #fetchData_results ~= moviesData
-               const fetchData_results = fetchData?.data?.results;
-               // #looping through all movieData, and taking movies that has video
-               const moviesData = [];
-               for (let i = 0; i < fetchData_results.length; i++) {
-                  await fetchMovieDetails(fetchData_results[i]).then((res) => {
-                     if (res.video) moviesData.push(res);
-                  });
+               try {
+                  const responseData = await fetchData(page);
+                  // #responseData_results ~= moviesData
+                  const responseData_results = responseData?.data?.results;
+                  // #looping through all movieData, and taking movies that has video
+                  const moviesData = [];
+                  for (let i = 0; i < responseData_results.length; i++) {
+                     await fetchMovieDetails(responseData_results[i]).then((res) => {
+                        if (res.video) moviesData.push(res);
+                     });
+                  }
+                  finalMoviesData = [...finalMoviesData, ...moviesData];
+                  // #inserting filtered moviesData into "responseData"(axios), to not loose the axios response data structure
+                  const tempResponseData = {
+                     ...responseData,
+                     data: { ...responseData?.data, results: finalMoviesData.slice(0, moviesData_limit) },
+                  };
+                  finalizedBannerData = { ...finalizedBannerData, ...tempResponseData };
+               } catch (error) {
+                  throw new Error("API failed...");
                }
-               finalMoviesData = [...finalMoviesData, ...moviesData];
-               // #inserting filtered moviesData into fetchData(axios), to not loose the axios response data structure
-               const tempFetchData = {
-                  ...fetchData,
-                  data: { ...fetchData?.data, results: finalMoviesData.slice(0, moviesData_limit) },
-               };
-               finalizedFetchData = { ...finalizedFetchData, ...tempFetchData };
 
                page++;
             } while (finalMoviesData?.length < moviesData_limit);
 
-            return finalizedFetchData;
+            return finalizedBannerData;
          }
 
          return axios_instance.get(url);
       },
       {
          staleTime: 5 * 60 * 1000, // #5 minutes
+         retry: 2, // Will retry failed requests 10 times before displaying an error (default value: 3)
       }
    );
 };
